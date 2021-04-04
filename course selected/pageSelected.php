fcc7af65393f5b1e480d393c16347f96de9bfbf0
DESKTOP-D53AFGR\kip <evansrotichh7@gmail.com>
2021-04-04 13:07:47 -0700

    first commit

---
+++ course selected/pageSelected.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>new</title>
+  
+  <!-- <link href="css/style.css" rel="stylesheet"> -->
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../css/mdb.dark.min.css"/>
+    <!-- <link rel="stylesheet" href="../login/css/style.css" /> -->
+</head>
+<body>
+
+<!-- Navbar -->
+    <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div id="logo">
+        <h1><a href="../index.html">COURS<span>ES</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+     <!--     <a href="index.html"><img src="assets/img/logo.png" alt=""></a> -->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="../index.html">Home</a></li>
+          <li><a class="nav-link scrollto" href="login.php">Login</a></li>
+          <li><a class="nav-link scrollto" href="register.php">Register</a></li>
+          <li><a class="nav-link scrollto" href="register.html"></a></li>
+          <li><a class="nav-link scrollto" href="register.html"></a></li>
+
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+   
+
+
+
+  <section>
+            <div class="text-center" >
+              <h4><strong>Pure medicine</strong></h4>
+            </div>
+
+            <section class="border p-4 mb-4">
+              <style>
+                .scrollspy-example {
+                  position: relative;
+                  height: 200px;
+                  overflow: auto;
+                }
+              </style>
+              <div class="row">
+                <div class="col-md-8">
+                  <div
+                    data-mdb-spy="scroll"
+                    data-mdb-target="#scrollspy"
+                    data-mdb-offset="0"
+                    class="scrollspy-example"
+                  >
+                    <section id="example-1">
+                      <h3>Description</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                    </section>
+                    <section id="example-2">
+                      <h3></h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                    </section>
+                    <section id="example-3">
+                      <h3></h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                      <section id="example-sub-A">
+                        <h3></h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Labore earum natus vel minima quod error maxime,
+                          molestias ut. Fuga dignissimos nisi nemo
+                          necessitatibus quisquam obcaecati et reiciendis
+                          quaerat accusamus numquam.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Labore earum natus vel minima quod error maxime,
+                          molestias ut. Fuga dignissimos nisi nemo
+                          necessitatibus quisquam obcaecati et reiciendis
+                          quaerat accusamus numquam.
+                        </p>
+                      </section>
+                      <section id="example-sub-B">
+                        <h3></h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Labore earum natus vel minima quod error maxime,
+                          molestias ut. Fuga dignissimos nisi nemo
+                          necessitatibus quisquam obcaecati et reiciendis
+                          quaerat accusamus numquam.
+                        </p>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Labore earum natus vel minima quod error maxime,
+                          molestias ut. Fuga dignissimos nisi nemo
+                          necessitatibus quisquam obcaecati et reiciendis
+                          quaerat accusamus numquam.
+                        </p>
+                      </section>
+                    </section>
+                    <section id="example-4">
+                      <h3></h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Labore earum natus vel minima quod error maxime,
+                        molestias ut. Fuga dignissimos nisi nemo necessitatibus
+                        quisquam obcaecati et reiciendis quaerat accusamus
+                        numquam.
+                      </p>
+                    </section>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div id="scrollspy" class="sticky-top">
+                    <ul class="nav flex-column nav-pills menu-sidebar">
+                      <li class="nav-item">
+                        <a class="nav-link active" href="#example-1"
+                          >Section 1</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#example-2">Section 2</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#example-3">Section 3</a>
+                        <ul class="nav flex-column ps-3">
+                          <li class="nav-item">
+                            <a class="nav-link" href="#example-sub-A"
+                              >Subsection A</a
+                            >
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#example-sub-B"
+                              >Subsection B</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#example-4">Section 4</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+
+
+            </section>
+          </section>
+          <!--Section: Scrollspy-->
+<!--Section: Summary-->
+                <section>
+                  <!-- <button type="button" class="btn btn-success btn-block mb-4">
+                    Place order
+                  </button> -->
+
+                  <div class="card">
+                    <div class="card-header bg-white text-center py-3">
+                      <h5 class="mb-0 fw-bold">Cluster Summary</h5>
+                    </div>
+
+                    <div class="card-body">
+                      <ul>
+                        <li><strong>kabarak university: </strong>44.7</li>
+                        <li><strong>Univeristy of Nairobi: </strong>45.8</li>
+                        <li><strong>Moi University: </strong>49.6</li>
+                        <li><strong>kabarak university: </strong>44.7</li>
+                        <li><strong>Univeristy of Nairobi: </strong>45.8</li>
+                        <li><strong>Moi University: </strong>49.6</li>
+                      </ul>
+                    </div>
+
+                    <div
+                      class="card-footer bg-white d-flex justify-content-between py-3"
+                    >
+                      <strong>Minimun cluster:</strong>
+                      <strong>44.2</strong>
+                    </div>
+                  </div>
+                </section>
+                <!--Section: Summary-->
+
+	   
+
+               
+
+
+</body>
+</html>
